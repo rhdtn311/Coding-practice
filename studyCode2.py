8c7b740563aa6215b542cd5b3270036ce65ea957
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-08-28 10:21:30 +0900

    Update studyCode2.py

--- studyCode2.py
+++ studyCode2.py
@@ -5,40 +5,44 @@ import requests
 # try, catch문을 활용했다.
 # 반복문에 대해 조금은 더 안 것 같다.
 # is 와 == 의 차이점에 대해 학습할 것이다. (is 는 객체가 같을 때 True, ==는 값이 같을 때 True)
-while True :
-  urlResult = input("Please write a URL or URLs you want to check. (seperated by comma)\n").split(",") 
 
-  starting = len(urlResult)
+# 훨씬 더 깔끔한 코드가 있다. if문과 함수를 적절히 잘 이용했다. 많이 배워야겠다.
+import os
+import requests
 
-  for j in urlResult :
-    j = j.strip()
-    if j[-4:] != ".com" : 
-      print("{} is not a valid answer.".format(j))
-      starting = 0 
-  
-  while starting > 0 :
-    for i in urlResult :  
-      starting -= 1    
-        
-      i = i.strip()
-      if i.lstrip("http://") == i : 
-        i = "http://" + i.lstrip("http://") 
-      try :      
-        r = requests.get(i)  
-        if r.status_code == 200 : 
-          print("{} is up!".format(i))
-        elif r.status_code != 200 : 
-          print("{} is down!".format(i))  
-      except : print("{} is down!".format(i))    
+def restart():
+  answer = str(input("Do you want to start over? y/n ")).lower()
+  if answer == "y" or answer =="n":
+    if answer == "n":
+        print("k. bye!")
+        return
+    elif answer == "y":
+      main()
+  else:
+    print("That's not a valid answer")
+    restart()
 
-  count = 1
-  while count > 0 :
-    answer = input("Do you want start over ? y/n\n") 
 
-    if answer == "y" : 
-      count -= 1
-    elif answer == "n" :
-      print("Ok. Bye.")
-      exit()
-    else : 
-      print("That's not a valid answer.")
+def main():
+  os.system('clear')
+  print("Welcome to IsItDown.py!\nPlease write a URL or URLs you want to check. (separated by comma)")
+  urls = str(input()).lower().split(",")
+  for url in urls:
+    url = url.strip()
+    if "." not in url:
+      print(url, "is not a valid URL.")
+    else:
+      if "http" not in url:
+        url = f"http://{url}"
+      try:
+        request = requests.get(url)
+        if request.status_code == 200:
+          print(url,"is up!")
+        else:
+          print(url, "is down!")
+      except:
+          print(url, "is down!")
+  restart()
+
+
+main()
