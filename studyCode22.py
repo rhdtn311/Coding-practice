36069f9993a45eec0c6821c63040232ed3fc291d
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-10-20 23:08:03 +0900

    Create studyCode22.py

---
+++ studyCode22.py
@@ -0,0 +1,22 @@
+# 코드업 2650 : 디지털 도어락 문제
+
+n,m,k = map(int,input().split())
+small = 0 
+array = []
+result = 0 
+
+array.append(n)
+array.append(m)
+array.append(k)
+array.sort()
+
+small = array[0]
+
+i = 1
+while i <= small :
+    if n % i == 0 and m % i == 0 and k % i == 0 :
+        result = i 
+
+    i+=1 
+
+print(result)
