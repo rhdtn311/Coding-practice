7ecad73e97d233a43bc16f662f82d13551cf0324
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-10-22 23:34:18 +0900

    Create studyCode24.py

---
+++ studyCode24.py
@@ -0,0 +1,13 @@
+# 백준 1026번 보물 문제
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+s = 0
+for i in range(n):
+    s += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
+
+print(s)
