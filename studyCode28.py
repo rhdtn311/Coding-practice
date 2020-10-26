c9e43626cbd8b5f00bbed8686b5859f32bcf459f
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-10-27 00:49:29 +0900

    Update studyCode28.py

--- studyCode28.py
+++ studyCode28.py
@@ -1,3 +1,5 @@
+# 코드업 3016: 1등한 학생의 성적 문제
+
 n = int(input())
 
 array = [] 
@@ -7,10 +9,10 @@ array3 = []
 for _ in range(n) : 
     array.append(input().split())
 
-min_index = 0
+max_index = 0
 for i in range(1,n) : 
-    if array[min_index][0] > array[i][0] :
-        min_index = i
+    if int(array[max_index][1]) < int(array[i][1]) :
+        max_index = i
 
 for j in range(n) : 
     array2.append(int(array[j][2]))
@@ -18,8 +20,8 @@ for j in range(n) :
 for k in range(n) : 
     array3.append(int(array[k][3]))
 
-array2.sort()
-array3.sort()
+array2.sort(reverse=True)
+array3.sort(reverse=True)
 
 for l in range(n) :
     for l2 in range(n) :
@@ -30,4 +32,5 @@ for m in range(n) :
     for m2 in range(n) :
         if array3[m] == int(array[m2][3]) : 
             array[m2][3] = m+1
-print(array)
+
+print(array[max_index][0],array[max_index][2],array[max_index][3])
