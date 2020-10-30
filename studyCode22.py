4ac0ca14bf49c894a508cac1143fcd2ec15e9054
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2020-10-30 15:39:35 +0900

    Update studyCode22.py

--- studyCode22.py
+++ studyCode22.py
@@ -1,10 +1,23 @@
-# 코드업 1090번 문제
+# 코드업 2650 : 디지털 도어락 문제
 
-a,r,n = map(int,input().split())
-count = 1
+n,m,k = map(int,input().split())
+small = 0 
+array = []
+result = 0 
 
-while count <= n-1 : 
-    a= a*r
-    count+=1
+array.append(n)
+array.append(m)
+array.append(k)
+array.sort()
 
-print(a)
+small = array[0]
+
+i = 1
+while i <= small :
+    if n % i == 0 and m % i == 0 and k % i == 0 :
+        result = i 
+
+    i+=1 
+
+print(result)
+
